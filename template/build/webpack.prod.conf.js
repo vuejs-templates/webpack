8fb182e916d012fe9fb7d11d6d5816b1cce911d3
Evan You <yyx990803@gmail.com>
2016-01-05 13:54:07 -0500

    fix production build css source maps

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -3,18 +3,31 @@ var config = require('./webpack.base.conf')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
+// naming output files with hashes for better caching.
+// dist/index.html will be auto-generated with correct URLs.
 config.output.filename = '[name].[chunkhash].js'
 config.output.chunkFilename = '[id].[chunkhash].js'
 
-config.devtool = 'source-map'
+// whether to generate source map for production files.
+// disabling this can speed up the build.
+var SOURCE_MAP = true
+
+config.devtool = SOURCE_MAP ? 'source-map' : false
+
+// generate loader string to be used with extract text plugin
+function generateExtractLoaders (loaders) {
+  return loaders.map(function (loader) {
+    return loader + '-loader' + (SOURCE_MAP ? '?sourceMap' : '')
+  }).join('!')
+}
 
 config.vue.loaders = {
   js: 'babel!eslint',
   // http://vuejs.github.io/vue-loader/configurations/extract-css.html
-  css: ExtractTextPlugin.extract('style-loader', 'css-loader'),
-  less: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
-  sass: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
-  stylus: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
+  css: ExtractTextPlugin.extract('style-loader', generateExtractLoaders(['css'])),
+  less: ExtractTextPlugin.extract('style-loader', generateExtractLoaders(['css', 'less'])),
+  sass: ExtractTextPlugin.extract('style-loader', generateExtractLoaders(['css', 'sass'])),
+  stylus: ExtractTextPlugin.extract('style-loader', generateExtractLoaders(['css', 'stylus']))
 }
 
 config.plugins = (config.plugins || []).concat([
