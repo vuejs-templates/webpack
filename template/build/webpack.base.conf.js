04b311ac9d1c2f72d8d18f898c301be14fdab652
Damian <rascada@users.noreply.github.com>
2016-01-14 22:18:59 +0100

    fix resolving loaders in linked packages

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -15,6 +15,11 @@ module.exports = {
       'src': path.resolve(__dirname, '../src')
     }
   },
+
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+
   module: {
     loaders: [
       {
