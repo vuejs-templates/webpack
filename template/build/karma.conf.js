3b475b0ec5eda0ee4b7bd247d101d3a98f00ada2
Eduardo San Martin Morote <i@posva.net>
2016-03-04 09:36:54 +0100

    Add coverage for components and js with isparta

--- template/build/karma.conf.js
+++ template/build/karma.conf.js
@@ -2,11 +2,40 @@ var webpackConf = require('./webpack.base.conf')
 delete webpackConf.entry
 webpackConf.devtool = 'inline-source-map'
 
+// Having a coverage tool makes sourcemaps pretty much useless
+// You can change this value to false for easier debugging
+var useCoverage = true
+if (useCoverage) {
+  webpackConf.vue = {
+    loaders: {
+      js: 'isparta'
+    }
+  }
+
+  webpackConf.module.preLoaders.forEach(function (preLoader) {
+    if (preLoader.test.toString() === '/\\.js$/') {
+      preLoader.loader = `isparta!${preLoader.loader}`
+      preLoader.exclude = /(test|node_modules)/
+    }
+  })
+
+  webpackConf.module.preLoaders.push({
+    test: /\.js$/,
+    loader: 'eslint',
+    include: /test/
+  })
+
+  webpackConf.isparta = {
+    embedSource: true,
+    noAutoWrap: true
+  }
+}
+
 module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
     files: ['../test/unit/index.js'],
     preprocessors: {
       '../test/unit/index.js': ['webpack', 'sourcemap']
@@ -14,6 +43,10 @@ module.exports = function (config) {
     webpack: webpackConf,
     webpackMiddleware: {
       noInfo: true
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: '../coverage'
     }
   })
 }
--- template/test/unit/index.js
+++ template/test/unit/index.js
@@ -5,3 +5,11 @@ Function.prototype.bind = require('function-bind')
 // require all test files (files that ends with .spec.js)
 var testsContext = require.context('.', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
+
+// you can require all the js files from src to include empty coverages too
+// This is an example where we want to include our services in coverage reports
+// var servicesContext = require.context('../../src/services/', true)
+// servicesContext.keys().forEach(servicesContext)
+// This other example includes everything
+// var sourceContext = require.context('../../src', true)
+// sourceContext.keys().forEach(sourceContext)
