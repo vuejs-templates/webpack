97979f3c60e6ec3f48d076c8ef8b4d32b022e548
Evgeny <EOlonov@gmail.com>
2016-04-01 09:52:28 +0300

    use resource

--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import resource from 'vue-resource'
+
+Vue.use(resource)
+
+export const Resource = Vue.resource.bind(Vue)
 
 /* eslint-disable no-new */
 new Vue({
