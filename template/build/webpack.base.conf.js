7d09f05c966b32a06b4704034c63376e69361003
Evan You <yyx990803@gmail.com>
2016-04-07 16:01:45 -0400

    add resolve alias for assets and components

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -16,7 +16,9 @@ module.exports = {
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, '../node_modules')],
     alias: {
-      'src': path.resolve(__dirname, '../src')
+      'src': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets'),
+      'components': path.resolve(__dirname, '../src/components')
     }
   },
   resolveLoader: {
