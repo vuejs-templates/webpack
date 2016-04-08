bbd35cedd23409c134c2afbabb4133151f7ddbc4
xinchaobeta <xinchaobeta@users.noreply.github.com>
2016-04-08 17:23:42 +0800

    exclude `build` directory when eslint check
    The `build` directory should not be included in the eslint check. Though the current config will not cause any error, but when using eslint-plugin-import will throw error: 
    
    `Unable to resolve path to module 'webpack-hot-middleware/client?noInfo=true&reload=true'`

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -36,7 +36,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'eslint',
         include: projectRoot,
-        exclude: /node_modules/
+        exclude: /(?:node_modules|\/build\/)/
       }
     ],
     loaders: [
