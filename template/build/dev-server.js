04ce9023f2bc08f3a7aead95ac4367d63b830e4c
moonqqqq <xy.jason.liu@gmail.com>
2016-04-12 16:49:22 -0400

    fix static path for windows environment (#77)
    After taking with @myst729, I use some sentience from him:
    Using back slash (\) as file separator is how path.join works in Windows environment, which makes loading of static files failed on my windows machine.
    In Mac or Linux it should be slash (/), which works.
    I think url.resolve is a better approach here.

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var url = require('url')
 var express = require('express')
 var webpack = require('webpack')
 var config = require('../config')
@@ -53,7 +54,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+var staticPath = url.resolve(config.build.assetsPublicPath, config.build.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
 module.exports = app.listen(port, function (err) {
