c7d6f9842695637a6b71c14c6668968b750839ef
Evan You <yyx990803@gmail.com>
2016-04-16 16:30:43 -0400

    make e2e pass

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -8,7 +8,7 @@ module.exports = {
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.logo')
       .assert.containsText('h1', 'Hello World!')
-      .assert.elementCount('p', 3)
+      .assert.elementCount('p', 2)
       .end()
   }
 }
