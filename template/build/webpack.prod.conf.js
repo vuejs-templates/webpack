39ac46f2138bc28a7b3e87a876478a9456543871
szrenwei <371151833@qq.com>
2016-06-03 03:08:45 +0800

    only bundle js files in vendor.js (#143)

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -65,6 +65,7 @@ module.exports = merge(baseWebpackConfig, {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
+          /\.js$/.test(module.resource) &&
           module.resource.indexOf(
             path.join(__dirname, '../node_modules')
           ) === 0
