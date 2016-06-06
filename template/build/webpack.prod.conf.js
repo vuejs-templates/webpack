7232706d0fba42fea5e6a03df18ea132f9bbf6fc
Chris Fritz <chrisvfritz@gmail.com>
2016-06-06 10:42:20 -0400

    add optional gzip compression for prod build (#150)

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -80,3 +80,23 @@ module.exports = merge(baseWebpackConfig, {
     })
   ]
 })
+
+if (config.build.productionGzip) {
+  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+  webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: new RegExp(
+        '\\.(' +
+        config.build.productionGzipExtensions.join('|') +
+        ')$'
+      ),
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  )
+}
+
+module.exports = webpackConfig
