cbcba9268dbfb277497bcdde6409dab4398eed8e
Nick Spiel <email@nickspiel.me>
2016-07-12 10:02:00 +1000

    Conditional server path (#188)
    * Pulling path config vars from dev instead of build
    
    * publicPath is now conditional based on environment
    
    * Added path properties to dev config

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -53,7 +53,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
 module.exports = app.listen(port, function (err) {
--- template/config/index.js
+++ template/config/index.js
@@ -19,6 +19,8 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     proxyTable: {}
   }
 }
