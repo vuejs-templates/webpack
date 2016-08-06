3508f23e0f1f5aca28b2c435b4957052ac942d89
Sean Goresht <seangores@gmail.com>
2016-08-06 01:53:01 -0600

    Refactor/reformat webpack dev conf

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -1,34 +1,36 @@
-var config = require('../config')
-var webpack = require('webpack')
-var merge = require('webpack-merge')
-var utils = require('./utils')
-var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+"use strict";
+
+const config = require("../config");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const utils = require("./utils");
+const baseWebpackConfig = require("./webpack.base.conf");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
+Object.keys(baseWebpackConfig.entry).forEach((name) => {
+	baseWebpackConfig.entry[name] = ["./build/dev-client"].concat(baseWebpackConfig.entry[name]);
+});
 
 module.exports = merge(baseWebpackConfig, {
-  module: {
-    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
-  },
-  // eval-source-map is faster for development
-  devtool: '#eval-source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
-    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    })
-  ]
-})
+	module: {
+		loaders: utils.styleLoaders({sourceMap: config.dev.cssSourceMap})
+	},
+	// eval-source-map is faster for development
+	devtool: "#eval-source-map",
+	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": config.dev.env
+		}),
+		// https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		// https://github.com/ampedandwired/html-webpack-plugin
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "index.html",
+			inject: true
+		})
+	]
+});
