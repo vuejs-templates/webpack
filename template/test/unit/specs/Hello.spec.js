c43bb4f17929047bb3db2aca35b5f894006b6de7
Thorsten Lünborg <t.luenborg@googlemail.com>
2016-09-08 17:04:41 +0200

    Fix #238 hello.spec.js lint for 1.0 (#252)
    * initial commit
    
    * forgot two lines

--- template/test/unit/specs/Hello.spec.js
+++ template/test/unit/specs/Hello.spec.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Hello from 'src/components/Hello'
+import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Hello from 'src/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
       template: '<div><hello></hello></div>',
-      components: { Hello }
-    }).$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent).to.contain('Hello World!')
-  })
-})
+      components: { Hello }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }).$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    expect(vm.$el.querySelector('.hello h1').textContent).to.contain('Hello World!'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
