7130bb9eebaab28e43ced5d380132d5db1e01ef5
Thorsten <t.luenborg@googlemail.com>
2016-09-11 23:37:21 +0200

    add support for process.env.PORT in night watch global

--- template/test/e2e/nightwatch.conf.js
+++ template/test/e2e/nightwatch.conf.js
@@ -23,7 +23,7 @@ module.exports = {
       "selenium_host": "localhost",
       "silent": true,
       "globals": {
-        "devServerURL": "http://localhost:" + config.dev.port
+        "devServerURL": "http://localhost:" + (process.env.PORT || config.dev.port)
       }
     },
 
