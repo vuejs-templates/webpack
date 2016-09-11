121eb35faf4a18fbc7ebe23c7c2436e22ef8ae34
Thorsten <t.luenborg@googlemail.com>
2016-09-11 21:15:50 +0200

    improve Message

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,7 @@
 {{#if_eq build "standalone"}}
-// the following line loads the standalone build instead of Runtime-only
-// thanks to an alias defined in /config/webpack.base.conf.js
-// So you don't have to do import Vue from 'vue/dist/vue'
+// The following line loads the standalone build of Vue instead of the runtime-only build,
+// so you don't have to do: import Vue from 'vue/dist/vue'
+// This is done with an alias defined in /config/webpack.base.conf.js
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
