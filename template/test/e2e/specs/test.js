41724181a006d1d6ef2224fb6a11b2a9b09d33bc
Thorsten <t.luenborg@googlemail.com>
2016-09-11 20:38:24 +0200

    provide devServer URL, especially the port from /config/index.js as a global in night watch's browser object

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -1,10 +1,15 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
+ // see nightwatch.conf.js
+ // default: http://localhost:8080
+ // automatically uses dev Server port from /config.index.js
+var devServer = browser.globals.devServerURL
+
 module.exports = {
   'default e2e tests': function (browser) {
     browser
-    .url('http://localhost:8080')
+    .url(devServer)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.logo')
       .assert.containsText('h1', 'Hello Vue!')
