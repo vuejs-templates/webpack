8f856192a3cc128a937bacb5f8a082a1b11a7fd5
Thorsten <t.luenborg@googlemail.com>
2016-09-11 20:46:27 +0200

    move variable assignment into the exported function b/c "browser" object is not avail outside.

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -1,13 +1,16 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
- // see nightwatch.conf.js
- // default: http://localhost:8080
- // automatically uses dev Server port from /config.index.js
-var devServer = browser.globals.devServerURL
+
 
 module.exports = {
   'default e2e tests': function (browser) {
+
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    var devServer = browser.globals.devServerURL
+
     browser
     .url(devServer)
       .waitForElementVisible('#app', 5000)
