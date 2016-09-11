d81b5b6ba44a108f03429ef565b5a5991691e30a
Thorsten <t.luenborg@googlemail.com>
2016-09-11 22:20:11 +0200

    improve comment - now warning about importing standalone directly as it breaks vie-loader

--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,7 @@
 {{#if_eq build "standalone"}}
 // The following line loads the standalone build of Vue instead of the runtime-only build,
 // so you don't have to do: import Vue from 'vue/dist/vue'
+// (also, importing Vue standalone this way breaks vue-loader, so don't do it)
 // This is done with an alias defined in /config/webpack.base.conf.js
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
