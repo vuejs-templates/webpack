009e91aad4c0778dbca44112ed00c9abcc39da35
Thorsten <t.luenborg@googlemail.com>
2016-09-11 21:05:31 +0200

    Add comment to main.js informing the user about the aliased behavior of "import Vue from 'due'" if standalone build was selected.

--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,8 @@
+{{if_eq build "standalone"}}
+// the following line loads the standalone build instead of Runtime-only
+// thanks to an alias defined in /config/webpack.base.conf.js
+// So you don't have to do import Vue from 'vue/dist/vue'
+{{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
