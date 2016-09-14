ed26a8b385373973484000f92584f23134da88e9
Thorsten Lünborg <t.luenborg@googlemail.com>
2016-09-14 16:42:14 +0200

    Add select option to init process: Standalone or runtime-only? (solve #242) (#256)
    * add election between standalone and runtime-only to meta.json
    
    * add alias to webpack con if standalone version was selected
    
    * Add comment to main.js informing the user about the aliased behavior of "import Vue from 'due'" if standalone build was selected.

--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,9 @@
+{{#if_eq build "standalone"}}
+// The following line loads the standalone build of Vue instead of the runtime-only build,
+// so you don't have to do: import Vue from 'vue/dist/vue'
+// (also, importing Vue standalone this way breaks vue-loader, so don't do it)
+// This is done with an alias defined in /build/webpack.base.conf.js
+{{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
