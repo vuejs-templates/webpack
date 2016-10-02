a705edde15a0c53c71162eafb58e4c90ded5ad3d
Markus Sorg <markus.sorg@gmail.com>
2016-10-02 03:07:03 +0200

    Open default browser on npm run dev (#292)

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -2,6 +2,7 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var config = require('../config')
+var opn = require('opn')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
@@ -61,5 +62,7 @@ module.exports = app.listen(port, function (err) {
     console.log(err)
     return
   }
-  console.log('Listening at http://localhost:' + port + '\n')
+  var uri = 'http://localhost:' + port
+  console.log('Listening at ' + uri + '\n')
+  opn(uri)
 })
