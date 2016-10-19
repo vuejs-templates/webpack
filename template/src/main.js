d96fd809882f61b6c3e93c0bab1e4e5f9bfd602f
Chris Fritz <chrisvfritz@gmail.com>
2016-10-18 22:53:24 -0400

    Update main.js (#327)

--- template/src/main.js
+++ template/src/main.js
@@ -8,5 +8,11 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
+  template: '<App/>',
+  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
