25aed12778d29897a4a0edb6029bbbfc7039b88c
Thorsten Lünborg <t.luenborg@googlemail.com>
2016-12-05 21:09:45 +0100

    fix condition
    it should be "standalone", since there is no alias when runtime is selectzd.

--- template/src/main.js
+++ template/src/main.js
@@ -1,4 +1,4 @@
-{{#if_eq build "runtime"}}
+{{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
