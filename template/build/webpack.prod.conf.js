da6cbe212aec8bcd2f55f4b0d9c4c92792266786
lmk123 <me@limingkai.cn>
2016-12-12 03:07:12 +0800

    use HashedModuleIdsPlugin to keep module ids consistent

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -6,6 +6,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HashedModuleIdsPlugin = require('./HashedModuleIdsPlugin')
 var env = {{#if_or unit e2e}}process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : {{/if_or}}config.build.env
@@ -36,7 +37,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         warnings: false
       }
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HashedModuleIdsPlugin(),
     // extract css into its own file
     new ExtractTextPlugin(utils.assetsPath('css/[name].[contenthash].css')),
     // generate dist index.html with correct asset hash for caching.
