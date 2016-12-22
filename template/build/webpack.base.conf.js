69f10742c3af6af118ba6d5465fa8e32101938e2
kgraff <kgraff@gsgenetics.com>
2016-12-22 17:32:42 -0500

    Move es-lint code to the webpack.dev.con.js as it should'nt be needed for production

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -35,22 +35,6 @@ module.exports = {
     fallback: [path.join(__dirname, '../node_modules')]
   },
   module: {
-    {{#lint}}
-    preLoaders: [
-      {
-        test: /\.vue$/,
-        loader: 'eslint',
-        include: projectRoot,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        include: projectRoot,
-        exclude: /node_modules/
-      }
-    ],
-    {{/lint}}
     loaders: [
       {
         test: /\.vue$/,
@@ -84,11 +68,6 @@ module.exports = {
       }
     ]
   },
-  {{#lint}}
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
-  {{/lint}}
   vue: {
     loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
     postcss: [
--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -1,9 +1,11 @@
+var path = require('path')
 var config = require('../config')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var projectRoot = path.resolve(__dirname, '../')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -12,8 +14,29 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 
 module.exports = merge(baseWebpackConfig, {
   module: {
+    {{#lint}}
+    preLoaders: [
+      {
+        test: /\.vue$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      }
+    ],
+    {{/lint}}
     loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
+  {{#lint}}
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+  {{/lint}}
   // eval-source-map is faster for development
   devtool: '#eval-source-map',
   plugins: [
