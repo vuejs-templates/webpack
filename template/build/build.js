62af09a3f55838e725eff687479724c58cf737e7
Miljan Aleksic <miljan@aleksic.es>
2016-12-23 22:37:23 +0800

    cleanup build moving common functions to utils

--- template/build/build.js
+++ template/build/build.js
@@ -4,9 +4,8 @@ require('shelljs/global')
 env.NODE_ENV = 'production'
 
 var path = require('path')
+var utils = require('./utils')
 var config = require('../config')
-var ora = require('ora')
-var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
 console.log(
@@ -15,22 +14,8 @@ console.log(
   '  Opening index.html over file:// won\'t work.\n'
 )
 
-var spinner = ora('building for production...')
-spinner.start()
-
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
-rm('-rf', assetsPath)
-mkdir('-p', assetsPath)
+utils.cleanPath(assetsPath)
 cp('-R', 'static/*', assetsPath)
 
-webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
-})
+utils.webpackBuild(webpackConfig, 'building for production...')
