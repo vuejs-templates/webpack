eae8c8dce116d628f374dfb6b7ff677dd75ae4d5
EGOIST <0x142857@gmail.com>
2016-12-29 17:41:05 +0800

    add friendly-errors-webpack-plugin

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -4,6 +4,7 @@ var merge = require('webpack-merge')
 var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrors = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -29,6 +30,7 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    })
+    }),
+    new FriendlyErrors()
   ]
 })
