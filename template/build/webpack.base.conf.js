557e831a7ea1141c87f94e8a815e3070e8c3bdd3
Patrick Arminio <patrick.arminio@gmail.com>
2017-01-22 00:22:54 +0000

    Remove json loader

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -66,10 +66,6 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
