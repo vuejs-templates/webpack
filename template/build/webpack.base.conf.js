4c5bea4fda26610c35cdb2e6b80a66ae0bd1772e
Patrick Arminio <patrick.arminio@gmail.com>
2017-01-22 00:33:16 +0000

    Fix syntax issue

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -99,5 +99,5 @@ module.exports = {
         ]
       }
     }
-  ]
+  })]
 }
--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -28,7 +28,7 @@ var webpackConfig = merge(baseWebpackConfig, {
           extract: true
         })
       }
-    },
+    }),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
