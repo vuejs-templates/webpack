efebb8a5fc6518d40c866b6bd80e37b3e7dc24e7
Patrick Arminio <patrick.arminio@gmail.com>
2017-01-22 00:17:38 +0000

    Update webpack dev settings

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -22,9 +22,8 @@ module.exports = merge(baseWebpackConfig, {
       'process.env': config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
