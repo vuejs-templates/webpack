f0da18776763c3a48fe3fe5da16793717833dd9e
F-loat <945852046@qq.com>
2017-01-25 10:14:51 +0800

    fix open browser always

--- template/config/index.js
+++ template/config/index.js
@@ -19,6 +19,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
+    autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
