1f529b2d4f201688e2622e26532a4279ed6c77b7
Ates Goral <ates.g@myplanet.com>
2017-01-29 22:41:48 -0500

    Support filter option for full control on path matching

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -41,7 +41,7 @@ Object.keys(proxyTable).forEach(function (context) {
   if (typeof options === 'string') {
     options = { target: options }
   }
-  app.use(proxyMiddleware(context, options))
+  app.use(proxyMiddleware(options.filter || context, options))
 })
 
 // handle fallback for HTML5 history API
