e53a87cacff652bb49681483aa2aebf6b5ef62b6
CodinCat <a55951234@gmail.com>
2017-02-04 05:36:35 +0800

    Use #cheap-module-eval-source-map for development (#440)

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -15,8 +15,8 @@ module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
-  // eval-source-map is faster for development
-  devtool: '#eval-source-map',
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
