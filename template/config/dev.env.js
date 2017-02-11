5df8ddf035ce533d4ccc445fb31cba84d8228666
Scott Molinari <scott.molinari@adduco.de>
2017-02-11 11:56:05 +0100

    Adds a flag, to allow the client dev to remove the production tips info, when in development.

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -2,5 +2,6 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  REMOVE_PROD_TIP: false
 })
