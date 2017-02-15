1e2e66a9f9457662fdffeeb35a4b1ff35d50ef4e
Maxime Colin <maxime@lahautesociete.com>
2017-02-15 16:32:55 +0100

    Changed : Skip interpolation on *.vue + /sprites/**

--- meta.js
+++ meta.js
@@ -1,21 +1,16 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
-      if (v1 || v2) {
-        return options.fn(this);
-      }
-
-      return options.inverse(this);
-    }
-  },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
+    "helpers":           {},
+    "prompts":           {
+        "name": {
+            "type":     "string",
+            "required": true,
+            "message":  "Project name"
+        },
     },
-  },
-  "filters": {
-  },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev"
+    "filters":           {},
+    "skipInterpolation": [
+        "src/sprites/templates/**/*",
+        "src/**/*.vue"
+    ],
+    "completeMessage":   "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev"
 };
