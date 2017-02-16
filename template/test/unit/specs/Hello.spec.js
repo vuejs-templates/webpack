49644c0909a0b6e77ad7cd2810d7fe14fb778f7e
Callum Macrae <callum@samknows.com>
2017-02-16 14:57:43 +0000

    Use vue-test for component testing

--- template/test/unit/specs/Hello.spec.js
+++ template/test/unit/specs/Hello.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import { mount } from 'vue-test'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Hello from 'src/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    const vm = new Constructor().$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    const el = mount(Hello){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    expect(el.find('h1').text())
       .to.equal('Welcome to Your Vue.js App'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
