17876a4778f8ca4d2463cda177604a748dd3e047
Evan You <yyx990803@gmail.com>
2017-02-18 22:27:37 -0500

    minimize css in code-split chunk as well

--- template/build/utils.js
+++ template/build/utils.js
@@ -11,8 +11,13 @@ exports.assetsPath = function (_path) {
 
 exports.cssLoaders = function (options) {
   options = options || {}
+  var isProduction = process.env.NODE_ENV === 'production'
+  var cssLoader = 'css' + (isProduction ? '?minimize' : '')
+
   // generate loader string to be used with extract text plugin
-  function generateLoaders (loaders) {
+  function generateLoaders (loader) {
+    var loaders = [cssLoader]
+    if (loader) loaders.push(loader)
     var sourceLoader = loaders.map(function (loader) {
       var extraParamChar
       if (/\?/.test(loader)) {
@@ -23,7 +28,7 @@ exports.cssLoaders = function (options) {
         extraParamChar = '?'
       }
       return loader + (options.sourceMap ? extraParamChar + 'sourceMap' : '')
-    }).join('!')
+    })
 
     // Extract CSS when that option is specified
     // (which is the case during production build)
@@ -33,19 +38,19 @@ exports.cssLoaders = function (options) {
         fallback: 'vue-style-loader'
       })
     } else {
-      return ['vue-style-loader', sourceLoader].join('!')
+      return ['vue-style-loader'].concat(sourceLoader)
     }
   }
 
   // http://vuejs.github.io/vue-loader/en/configurations/extract-css.html
   return {
-    css: generateLoaders(['css']),
-    postcss: generateLoaders(['css']),
-    less: generateLoaders(['css', 'less']),
-    sass: generateLoaders(['css', 'sass?indentedSyntax']),
-    scss: generateLoaders(['css', 'sass']),
-    stylus: generateLoaders(['css', 'stylus']),
-    styl: generateLoaders(['css', 'stylus'])
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass?indentedSyntax'),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
   }
 }
 
