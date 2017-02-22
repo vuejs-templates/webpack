86f757d2c73951c43275c255f87f6804f01f558a
stylehuan <fhuanweb@gmail.com>
2017-02-22 17:13:38 +0800

    update some fiel

--- template/src/router/routerInstance.js
+++ template/src/router/routerInstance.js
@@ -1,7 +1,6 @@
 import VueRouter from 'vue-router'
 import routerConf from "./router"
-import {eventBus, SYSTEM_Msg} from "../events/systemEvent"
-
+import {SystemEvent, MSG} from "../events/systemEvent"
 const routes = [
     {
         path: routerConf.root,
@@ -10,22 +9,17 @@ const routes = [
         }
     }
 ];
-
 const router = new VueRouter({
     // mode: 'history',//IOS低版本要开启history模式
     routes,
     transitionOnLoad: false
 });
-
-
 router.beforeEach((to, from, next) => {
-    eventBus.emit(SYSTEM_Msg.ROUTER_BEFORE);
+    SystemEvent.emit(MSG.ROUTER_BEFORE);
     setTimeout(next, 200);
 });
-
 router.afterEach(() => {
     console.log("完成");
-    eventBus.emit(SYSTEM_Msg.ROUTER_AFTER);
+    SystemEvent.emit(MSG.ROUTER_AFTER);
 });
-
 export default router;
