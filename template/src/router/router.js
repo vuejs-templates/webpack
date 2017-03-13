d8634be934376d3da47fbe0d295d7e50b3e00d01
young <ohoh.co@qq.com>
2017-03-13 16:07:18 +0800

    upgrade router

--- template/src/router/router.js
+++ template/src/router/router.js
@@ -2,8 +2,12 @@ import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import routes from './routes'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
-export default new Router({
+let router =  new Router({
+  mode: 'history',
+  base: __dirname,
+  // linkActiveClass: 'actived',
+  scrollBehavior: () => ({ y: 0 }),
   routes{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+export default router
