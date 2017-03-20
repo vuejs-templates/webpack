d49c5745ae145ce12f860a1662e2f3d5f05e17d0
Warat Wongmaneekit <canopybanj@gmail.com>
2017-03-20 20:24:06 +0700

    Add mapGetter to hello component

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>\{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>\{{ subWelcomeMsg }}</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -21,13 +21,22 @@
 </template>
 
 <script>
+{{#vuex}}
+import { mapGetters } from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/vuex}}
+
 export default {
   name: 'hello',
   data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
     return {
       msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  }{{#vuex}},
+  computed: {
+    ...mapGetters([
+      'subWelcomeMsg'
+    ]){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  }{{/vuex}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
