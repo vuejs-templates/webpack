031d0277f14b91b76bf57baf9895b6f263881e60
Toilal <toilal.dev@gmail.com>
2017-03-20 14:54:46 +0100

    Fix tslint issues on types definition file

--- template/src/typings.d.ts
+++ template/src/typings.d.ts
@@ -1,30 +1,31 @@
 // vue-template-loader (@see https://github.com/ktsn/vue-template-loader)
-declare module '*.html' {
-  import Vue from 'vue';
+/* tslint:disable:interface-name */
+declare module "*.html" {
+  import Vue from "vue";
   interface WithRender {
-    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>
-    <V extends typeof Vue>(component: V): V
+    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>;
+    <V extends typeof Vue>(component: V): V;
   }
   const withRender: WithRender;
-  export = withRender
+  export = withRender;
 }
 
-declare module '*.scss' {
-  import Vue from 'vue';
+declare module "*.scss" {
+  import Vue from "vue";
   interface WithRender {
-    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>
-    <V extends typeof Vue>(component: V): V
+    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>;
+    <V extends typeof Vue>(component: V): V;
   }
   const withRender: WithRender;
-  export = withRender
+  export = withRender;
 }
 
-declare module '*.css' {
-  import Vue from 'vue';
+declare module "*.css" {
+  import Vue from "vue";
   interface WithRender {
-    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>
-    <V extends typeof Vue>(component: V): V
+    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>;
+    <V extends typeof Vue>(component: V): V;
   }
   const withRender: WithRender;
-  export = withRender
+  export = withRender;
 }
