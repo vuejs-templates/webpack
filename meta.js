316b6358382991c34f1576bf068eba8a68426ec0
Warat Wongmaneekit <canopybanj@gmail.com>
2017-03-20 17:46:42 +0700

    Add vuex Question

--- meta.js
+++ meta.js
@@ -44,6 +44,10 @@ module.exports = {
       "type": "confirm",
       "message": "Install vue-router?"
     },
+    "vuex": {
+      "type": "confirm",
+      "message": "Install vuex?"
+    },
     "lint": {
       "type": "confirm",
       "message": "Use ESLint to lint your code?"
