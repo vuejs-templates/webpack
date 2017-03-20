1635947241ca290328f20f9153b60b543abbd81d
Warat Wongmaneekit <canopybanj@gmail.com>
2017-03-20 18:46:00 +0700

    filter store folder when vuex disable

--- meta.js
+++ meta.js
@@ -90,7 +90,8 @@ module.exports = {
     "test/unit/**/*": "unit",
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+    "src/router/**/*": "router",
+    "src/store/**/*": "vuex"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
