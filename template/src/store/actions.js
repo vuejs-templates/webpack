caae7b8e7c0f26e4d5146c15573ad34b154ea36d
Warat Wongmaneekit <canopybanj@gmail.com>
2017-03-21 00:24:17 +0700

    Add test for actions

--- template/src/store/actions.js
+++ template/src/store/actions.js
@@ -1,6 +1,6 @@
 const actions = {
-  changeWelcomeMsg: ({ commit }, newWelcomeMsg) => {
-    commit('changeWelcomeMsg', newWelcomeMsg){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  changeSubWelcomeMsg: ({ commit }, newWelcomeMsg) => {
+    commit('changeSubWelcomeMsg', newWelcomeMsg){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 export default actions{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
