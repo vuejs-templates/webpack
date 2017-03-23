d399afa403273862da2420881f01eba00aca47ae
Evan You <yyx990803@gmail.com>
2017-03-23 16:43:13 +0800

    adjust dev server configuration

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -10,7 +10,7 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 10000)
+      .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.hello')
       .assert.containsText('h1', 'Welcome to Your Vue.js App')
       .assert.elementCount('img', 1)
