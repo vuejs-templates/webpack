3948351f115628977036e5ec1cfd582d560433af
Aspectis <aspectis@users.noreply.github.com>
2017-03-24 00:42:20 +0100

    Use single quotes

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -33,7 +33,7 @@ module.exports = {
       {
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
-        enforce: "pre",
+        enforce: 'pre',
         include: [resolve('src'), resolve('test')],
         options: {
           formatter: require('eslint-friendly-formatter')
