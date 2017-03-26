f78054e9d3d40fcf5ebf3224b6117c4a46d8c097
汪磊 <ice@micua.com>
2017-03-27 05:05:01 +0800

    Update url-loader config (#590)
    > Rule.query only exists for compatibility reasons. Use Rule.options instead.

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -53,7 +53,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
@@ -61,7 +61,7 @@ module.exports = {
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
