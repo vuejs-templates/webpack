eb29a2bc501f471a15d540fc4182591a5f028c47
Damir Vandic <info@dvic.io>
2017-04-04 21:35:11 +0200

    fix tslint

--- template/test/unit/typings.d.ts
+++ template/test/unit/typings.d.ts
@@ -1,3 +1,3 @@
 interface WebpackRequire extends NodeRequire {
-  context(file: string, flag?: boolean, exp?: RegExp): any{{#if_eq tslintConfig "airbnb"}};{{/if_eq}}
+  context (file: string, flag?: boolean, exp?: RegExp): any{{#if_eq tslintConfig "airbnb"}};{{/if_eq}}
 }
