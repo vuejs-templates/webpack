55edf523126f97e443c51aed0dbac5324adc59bb
Thorsten Lünborg <t.luenborg@googlemail.com>
2017-04-19 10:11:45 +0200

    Update webpack.test.conf.js
    add code comments.

--- template/build/webpack.test.conf.js
+++ template/build/webpack.test.conf.js
@@ -13,6 +13,8 @@ var webpackConfig = merge(baseConfig, {
   devtool: '#inline-source-map',
   resolveLoader: {
     alias: {
+      // necessary to to make lang="scss" work in test when using vue-loader's ?inject option 
+      // see discussion at https://github.com/vuejs/vue-loader/issues/724
       'scss-loader': 'sass-loader'
     }
   },
