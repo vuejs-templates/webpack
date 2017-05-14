ea821d8e1a83fadcd1f54f97070e0a5d86580d41
Konstantin <2447785@gmail.com>
2017-05-14 09:46:52 +0300

    Fix base template render

--- template/format_index_html.py
+++ template/format_index_html.py
@@ -6,7 +6,7 @@ file = 'templates/index.html'
 with open(file, "r+") as f:
     s = f.read()
     f.seek(0)
-    f.write("{ % load staticfiles % }\n" + s)
+    f.write("{% load staticfiles %}\n" + s)
 
 for i, line in enumerate(fileinput.input(file, inplace=1)):
     sys.stdout.write(line.replace('href=//', "href={% static '"))
