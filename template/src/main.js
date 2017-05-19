647ff6026d98d5e389afa6ffefe3f6fc8d72ec2a
John Leider <john.j.leider@gmail.com>
2017-05-18 22:33:00 -0400

    init

--- template/src/main.js
+++ template/src/main.js
@@ -3,11 +3,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vuetify from 'vuetify'
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
