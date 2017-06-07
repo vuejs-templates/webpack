d8e0b6fa7c446f8267fb03caeb810730aa8cf46d
Peter Schröder <phoetmail@googlemail.com>
2017-06-07 11:41:16 +0200

    remove the wait

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -28,7 +28,7 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 5000)
+      .waitForElementVisible('#app')
       .assert.elementPresent('.hello')
       .assert.containsText('h1', 'Welcome to Your Vue.js App')
       .assert.elementCount('img', 1)
