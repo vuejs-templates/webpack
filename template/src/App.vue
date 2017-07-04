20e0a106d1a9016ef7349082c78b6bb3c06bc0d7
John Leider <john.j.leider@gmail.com>
2017-07-04 11:14:29 -0400

    updated for 0.13

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <v-app>
+  <v-app light>
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
+      enable-resize-watcher
     >
       <v-list>
-        <v-list-item
+        <v-list-tile
+          value="true"
           v-for="(item, i) in items"
           :key="i"
         >
-          <v-list-tile value="true">
-            <v-list-tile-action>
-              <v-icon light v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
+    <v-toolbar fixed>
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
       <v-btn
         icon
@@ -78,14 +78,12 @@
       v-model="rightDrawer"
     >
       <v-list>
-        <v-list-item>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list-item>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed">
