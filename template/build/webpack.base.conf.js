9992f5a058c53f32b03d9a8743612188c04698af
Ffloriel <Ffloriel@users.noreply.github.com>
2017-07-13 11:20:06 +0200

    Video audio loader (#765)
    * gitignore vscode
    
    * media loader
    
    audio and video tag
    
    * Remove vscode gitignore
    
    * Add \n back
    
    * fix missing closing bracket
    
    * add default values (img, image)

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -59,6 +59,14 @@ module.exports = {
         }
       },
       {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
