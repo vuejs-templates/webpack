e4ebec9115617b93fdf2f3cabbaafb822bb81e7a
刘祺 <qil@jumei.com>
2017-08-07 18:34:50 +0800

    fix meta.js

--- meta.js
+++ meta.js
@@ -1,4 +1,13 @@
 module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
   "prompts": {
     "name": {
       "type": "string",
@@ -15,6 +24,26 @@ module.exports = {
       "type": "string",
       "message": "Author"
     },
+    "build": {
+      "type": "list",
+      "message": "Vue build",
+      "choices": [
+        {
+          "name": "Runtime + Compiler: recommended for most users",
+          "value": "standalone",
+          "short": "standalone"
+        },
+        {
+          "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
+          "value": "runtime",
+          "short": "runtime"
+        }
+      ]
+    },
+    "router": {
+      "type": "confirm",
+      "message": "Install vue-router?"
+    },
     "eslint": {
       "type": "confirm",
       "message": "Use ESLint to lint your code?"
@@ -30,9 +59,9 @@ module.exports = {
           "short": "Standard"
         },
         {
-          "name": "AirBNB (https://github.com/airbnb/javascript)",
+          "name": "Airbnb (https://github.com/airbnb/javascript)",
           "value": "airbnb",
-          "short": "AirBNB"
+          "short": "Airbnb"
         },
         {
           "name": "none (configure it yourself)",
@@ -73,9 +102,14 @@ module.exports = {
   },
   "filters": {
     ".eslintrc.js": "eslint",
+    ".eslintignore": "eslint",
     ".stylelintrc.js": "stylelint",
+    ".stylelintignore": "stylelint",
+    "config/test.env.js": "unit || e2e",
     "test/unit/**/*": "unit",
-    "test/e2e/**/*": "e2e"
+    "build/webpack.test.conf.js": "unit",
+    "test/e2e/**/*": "e2e",
+    "src/router/**/*": "router"
   },
-  "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
