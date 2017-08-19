02a6dbd51317c1fa925cd03bb02f88fc44ea33a0
薛定谔的猫 <hh_2013@foxmail.com>
2017-08-19 22:17:19 +0800

    Chore: del semi.

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -8,7 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var MinifyPlugin = require("babel-minify-webpack-plugin");
+var MinifyPlugin = require("babel-minify-webpack-plugin")
 var env = {{#if_or unit e2e}}process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : {{/if_or}}config.build.env
