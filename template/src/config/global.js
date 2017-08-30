ba47d9b2a69e318aac1b1e0a66f3a6cd4fd3e035
qintianxiang <txqin@wisedu.com>
2017-08-30 16:37:30 +0800

    load

--- template/src/config/global.js
+++ template/src/config/global.js
@@ -3,7 +3,7 @@ import {getConf} from 'services/config.service'
 let conf = {}
 
 export const load = () => {
-    getConf().then(data => {
+    return getConf().then(data => {
         Object.assign(conf, data)
         return conf
     })
