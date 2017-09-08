f825ef82b7bea44eabdd5db7f7f75129891b9b00
Vladislav Klimanov <ahmpro@rslayers.com>
2017-09-08 12:56:23 +0300

    showing actual server's hostname and port

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -67,7 +67,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = 'http://' + host + ':' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
