8c9fcaeaf73236aa8ecc8f5c6e29a8d73c0adc86
Tyler Peterson <tylermpeterson36@gmail.com>
2017-09-28 14:18:22 -0600

    Use multi-process parallel running and file cache to improve the build speed

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -35,7 +35,8 @@ var webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: true,
+      parallel: true
     }),
     // extract css into its own file
     new ExtractTextPlugin({
