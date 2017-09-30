cdea8fe0fd9aff5e0303cfecda3a362326e386d1
yibuyisheng <yibuyisheng@163.com>
2017-09-30 19:38:04 +0800

    userPostCSS config

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -13,7 +13,7 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
