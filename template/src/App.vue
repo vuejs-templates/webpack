70fec58d59f24cb8da512eb1b6295a8f19647955
Sam Wray <sam@wray.pro>
2017-10-06 16:50:42 +0100

    Add sass loader, fonts, kitty branding

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/kitty.png" v-rjs="2">
+    <transition name="slide-down">
+      <img src="@/../static/kitty.png" v-rjs="2" width="206" height="110" v-show="isMounted">
+    </transition>
     {{#router}}
     <router-view></router-view>
     {{else}}
@@ -10,25 +12,58 @@
 </template>
 
 <script>
-{{#unless router}}
-import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  {{#unless router}}
+  import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-{{/unless}}
-export default {
-  name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
-  components: {
-    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  {{/unless}}
+  export default {
+    name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
+    components: {
+      Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},{{/router}}
+    data() {
+      return {
+        isMounted: false{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      };
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    mounted() {
+      console.log('%cWe Are Kitty', 'background: #111111; color: #FF315D; padding: 5px 20px;'){{#if_eq lintConfig "airbnb"}};{{/if_eq}} // eslint-disable-line
+      this.isMounted = true{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url("https://fonts.googleapis.com/css?family=Work+Sans");
+  @font-face {
+    font-family: 'UniNeue-Regular';
+    src: url('~./assets/fonts/Fontfabric - UniNeueRegular.otf');
+    font-weight: 400;
+  }
+
+  html, body {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    background-color: #111;
+    color: #fff;
+    margin: 0;
+    position: relative;
+  }
+
+  #app {
+    font-family: "Work Sans", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    font-size: 16pt;
+  }
 </style>
--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,53 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>\{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <transition name="slide-up">
+    <div class="hello" v-show="isMounted">
+      <h1>\{{ msg }}</h1>
+      <h2>Essential Links</h2>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        <br>
+        <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+        <li><a href="https://vuejs.org/v2/style-guide/" target="_blank">Vue.js Style Guide</a></li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      </ul>
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
-    return {
-      msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  export default {
+    name: 'hello',
+    data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+      return {
+        msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+        isMounted: false{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    mounted{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+      this.isMounted = true{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  .hello {
+    line-height: 21pt;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-a {
-  color: #42b983;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  @keyframes kitty-gradient {
+      0% { background-position: 0% 50% }
+      100% { background-position: 100% 50% }
+  }
+
+  a {
+    color: #FF315D;
+    font-family: "UniNeue-Bold", sans-serif;
+    text-decoration: none;
+  }
+
+  a:hover {
+    background: linear-gradient(to right, #FF035B, #FF5930, #FF035B);
+    background-size: 400% 100%;
+    animation: kitty-gradient 1s linear infinite;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 </style>
