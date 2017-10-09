109ead8af6c575cc0adebbde993b2b0618f5f5a3
iminin <Igor.Minin@t-systems.ru>
2017-10-09 15:28:31 +0300

    Cleanup

--- meta.js
+++ meta.js
@@ -93,7 +93,7 @@ module.exports = {
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router",
     "tsconfig.json": "typescript",
-    "src/vue-shims.d.ts": "typescript"
+    "vue-shims.d.ts": "typescript"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack",
   "metalsmith": function (metalsmith, opts, helpers) {
