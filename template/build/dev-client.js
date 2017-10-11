be3efc020a15a802f810647f69c45bf1bdef1773
Thorsten Lünborg <t.luenborg@googlemail.com>
2017-10-11 09:31:01 +0200

    stick to ES5
    this file is not transpiled

--- template/build/dev-client.js
+++ template/build/dev-client.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 'use strict'
 require('eventsource-polyfill')
-const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
