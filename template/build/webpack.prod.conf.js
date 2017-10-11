19b89ebc3297f171014568ae5f32d870d5c4259f
Thorsten <t.luenborg@googlemail.com>
2017-10-12 01:40:32 +0200

    Barman!

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -95,6 +95,15 @@ const webpackConfig = merge(baseWebpackConfig, {
       name: 'manifest',
       chunks: ['vendor']
     }),
+    // This instance extracts shared chunks from code splitted chunks and bundles them
+    // in a separate chunk, similar to the vendor chunk
+    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'app',
+      async: true,
+      children: true,
+      minChnks: 3,
+    }),
     // copy custom static assets
     new CopyWebpackPlugin([
       {
