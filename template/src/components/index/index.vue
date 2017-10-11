090119e3791930f15d9d91db1df6f2ca1f943291
lanseria <zhangchao564265135@hotmail.com>
2017-10-11 12:05:21 +0800

    修复index.html 中的视图viewport在移动端的缩放显示

--- template/src/components/index/index.vue
+++ template/src/components/index/index.vue
@@ -13,19 +13,19 @@ export default {
 <style scoped>
 
 </style>
-{{/if_eq}}{{/router}}
+{{/if_eq}}
 {{#if_eq styls "less"}}
 <style lang="less" scoped>
 
 </style>
-{{/if_eq}}{{/router}}
+{{/if_eq}}
 {{#if_eq styls "sass"}}
 <style lang="sass" scoped>
 
 </style>
-{{/if_eq}}{{/router}}
+{{/if_eq}}
 {{#if_eq styls "stylus"}}
 <style lang="stylus" scoped>
 
 </style>
-{{/if_eq}}{{/router}}
+{{/if_eq}}
