e66b8369549468f1d00ffdecf62ded1c6e355204
iminin <Igor.Minin@t-systems.ru>
2017-10-16 13:38:44 +0300

    Removed vue shims

--- meta.js
+++ meta.js
@@ -92,8 +92,7 @@ module.exports = {
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router",
-    "tsconfig.json": "typescript",
-    "vue-shims.d.ts": "typescript"
+    "tsconfig.json": "typescript"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack",
   "metalsmith": function (metalsmith, opts, helpers) {
