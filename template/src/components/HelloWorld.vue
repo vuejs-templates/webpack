61382fa047c91bce7ec1ab1b7f68fd046582d0c9
dmaskew <dmaskew@homeadvisor.com>
2017-10-20 10:22:11 -0600

    Initial Commit

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ export default {
   name: 'HelloWorld',
   data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
     return {
-      msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+      msg: 'Welcome to Your Vue.js App for CRM'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
