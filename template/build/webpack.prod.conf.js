33108a136cbad011b505a8b1692995ce2a0e7918
SidKwok <oceankwok@hotmail.com>
2017-10-20 14:56:19 +0800

    replace `id` with `name` in chunk files, Fix #996

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -25,7 +25,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[name].[chunkhash].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
