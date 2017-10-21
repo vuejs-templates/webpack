4fcf2ec35007c69069f8af012a2d165c135334d3
wenfan <wenfan@malong.com>
2017-10-21 22:17:05 +0800

    remove test in repository

--- test/build/dev-client.js
+++
@@ -1,9 +0,0 @@
-import 'eventsource-polyfill';
-
-const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
-
-hotClient.subscribe((event) => {
-  if (event.action === 'reload') {
-    window.location.reload();
-  }
-});
--- test/config/index.js
+++
@@ -1,8 +0,0 @@
-// see http://vuejs-templates.github.io/webpack for documentation.
-import prodEnviroment from './prod.env';
-import devEnviroment from './dev.env';
-
-export default {
-  build: prodEnviroment,
-  dev: devEnviroment
-};
