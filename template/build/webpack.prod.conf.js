cf408f24bbbccdf374fef9d7a5bf4af5fae8a21c
L·Lawliet <553867163@qq.com>
2017-11-01 19:51:52 +0800

    fix the config contradiction (#1014)

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -47,9 +47,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: {
-        safe: true
-      }
+      cssProcessorOptions: config.build.productionSourceMap
+      ? { safe: true, map: { inline: false } }
+      : { safe: true }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
