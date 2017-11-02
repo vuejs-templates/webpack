03fe67fddbee10f14115a4bdbf340cc52920f17d
Mgen <m@mgenware.com>
2017-11-03 00:58:36 +0800

    When using CommonsChunkPlugin and there are extracted chunks (from ExtractTextPlugin.extract) in the commons chunk, allChunks must be set to true https://github.com/webpack-contrib/extract-text-webpack-plugin

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -42,7 +42,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].[contenthash].css'),
+      allChunks: true,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
