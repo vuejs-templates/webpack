aba0f9c7b7903a5371469ecbd80b6f7df93fe536
Thorsten Lünborg <t.luenborg@googlemail.com>
2017-11-08 21:24:44 +0100

    Switch to webpack-dev-server (close #964) (#975)
    * Finished testable version.
    
    * close #960
    
    * fix node-notifier version
    
    * remove console.log
    
    * moved general dependency out of unit test-only dependency block.
    
    * fix typo
    
    * ignore /test folder
    
    * make HMR work correctly.
    
    * improve console messages for HMR - now show filenames of replaced modules.
    
    * fix typo in eslint-loader config
    
    * move imports for the env files from /config/index.js directly into the webpack config.
    
    Reasoning: thosen file imports are not configuration, so they don't belong inside of config/index.js
    
    * fix wrong overlay config

--- template/build/utils.js
+++ template/build/utils.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const config = require('../config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const pkg = require('../package.json')
 
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
@@ -69,4 +70,23 @@ exports.styleLoaders = function (options) {
     })
   }
   return output
+}
+
+exports.createNotifierCallback = function () {
+  const notifier = require('node-notifier')
+
+  return (severity, errors) => {
+    if (severity !== 'error') {
+      return
+    }
+    const error = errors[0]
+
+    const filename = error.file.split('!').pop()
+    notifier.notify({
+      title: pkg.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    })
+  }
 }
--- template/test/e2e/runner.js
+++ template/test/e2e/runner.js
@@ -1,8 +1,23 @@
 // 1. start the dev server using production config
 process.env.NODE_ENV = 'testing'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-var server = require('../../build/dev-server.js'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-server.ready.then(() => {
+const webpack = require('webpack'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+const DevServer = require('webpack-dev-server'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+const webpackConfig = require('../../build/webpack.prod.conf'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+const devConfigPromise = require('../../build/webpack.dev.conf'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+let server{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+devConfigPromise.then(devConfig => {
+  const devServerOptions = devConfig.devServer{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const compiler = webpack(webpackConfig){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  server = new DevServer(compiler, devServerOptions){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const port = devServerOptions.port{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const host = devServerOptions.host{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  return server.listen(port, host){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+})
+.then(() => {
   // 2. run the nightwatch test suite against it
   // to run in additional browsers:
   //    1. add an entry in test/e2e/nightwatch.conf.json under "test_settings"
@@ -10,7 +25,7 @@ server.ready.then(() => {
   // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
   // For more information on Nightwatch's config file, see
   // http://nightwatchjs.org/guide#settings-file
-  var opts = process.argv.slice(2){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  let opts = process.argv.slice(2){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   if (opts.indexOf('--config') === -1) {
     opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }
@@ -18,8 +33,8 @@ server.ready.then(() => {
     opts = opts.concat(['--env', 'chrome']){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }
 
-  var spawn = require('cross-spawn'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const spawn = require('cross-spawn'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
   runner.on('exit', function (code) {
     server.close(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
