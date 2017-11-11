64be9f727f4efbc63c83925f37696533d6f8e223
Thorsten <t.luenborg@googlemail.com>
2017-11-11 13:15:30 +0100

    fix condition, should als fail on `undefined`

--- template/build/utils.js
+++ template/build/utils.js
@@ -31,7 +31,7 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS !== false ? [cssLoader, postcssLoader] : [cssLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
