888a627a4e3b61b00c1bf4f0756c6b0f6524a3e9
Ryan Liu <m@mgenware.com>
2017-11-12 22:52:47 +0800

    Use webpack 3's standard way to generate a manifest in CommonsChunkPlugin (#1028)
    The doc URL: https://webpack.js.org/plugins/commons-chunk-plugin/#manifest-file
    * `minChunks` is set to `Infinity` so that only webpack runtime is written to manifest file.
    * `chunnks: ['vendor']` is removed as entry chunks are selected if `chunks` is omitted.

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -93,7 +93,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
-      chunks: ['vendor']
+      minChunks: Infinity,
     }),
     // This instance extracts shared chunks from code splitted chunks and bundles them
     // in a separate chunk, similar to the vendor chunk
