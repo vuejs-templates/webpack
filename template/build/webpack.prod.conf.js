10621e8f533ff9bd79d61bd3d773b7682699125f
Thorsten Lünborg <t.luenborg@googlemail.com>
2017-11-12 18:49:30 +0100

    Update webpack.prod.conf.js

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -43,9 +43,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // set the following options to `true` if you want to extract CSS from
+      // set the following option to `true` if you want to extract CSS from
       // codesplit chunks into this main css file as well.
-      // this will result in *all* of your app's CSS being loaded upfront.
+      // This will result in *all* of your app's CSS being loaded upfront.
       allChunks: false,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
