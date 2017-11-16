b14a70adc208837fd2953a9deeedabab64e40217
Thorsten <t.luenborg@googlemail.com>
2017-11-16 22:37:20 +0100

    make jest ignore the e2e test folder because its likely to contain files matching the jest's filename pattern

--- template/test/unit/jest.conf.js
+++ template/test/unit/jest.conf.js
@@ -13,7 +13,10 @@ module.exports = {
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
-  },
+  },{{#e2e}}
+  testPathIgnorePatterns: [
+    '<rootDir>/test/e2e'
+  ],{{/e2e}}
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
