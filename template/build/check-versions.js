f90716f107a6632846297c0628d2153b6c00c790
Emil Valeev <emil.valeev@emlid.com>
2017-11-17 23:12:23 +0300

    check-verstions: Add some line breaks to improve code readability

--- template/build/check-versions.js
+++ template/build/check-versions.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const semver = require('semver')
 const packageConfig = require('../package.json')
 const shell = require('shelljs')
+
 function exec (cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
@@ -25,8 +26,10 @@ if (shell.which('npm')) {
 
 module.exports = function () {
   const warnings = []
+
   for (let i = 0; i < versionRequirements.length; i++) {
     const mod = versionRequirements[i]
+
     if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
       warnings.push(mod.name + ': ' +
         chalk.red(mod.currentVersion) + ' should be ' +
@@ -39,10 +42,12 @@ module.exports = function () {
     console.log('')
     console.log(chalk.yellow('To use this template, you must update following to modules:'))
     console.log()
+
     for (let i = 0; i < warnings.length; i++) {
       const warning = warnings[i]
       console.log('  ' + warning)
     }
+
     console.log()
     process.exit(1)
   }
