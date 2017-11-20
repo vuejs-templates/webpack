5bc2b0b62a32e80bd0af747b4c53cf147abbd1f8
iminin <Igor.Minin@t-systems.ru>
2017-11-20 14:49:08 +0300

    Vue shims moved to root for Karma to work properly, Jest support added

--- template/test/unit/specs/HelloWorld.spec.js
+++ template/test/unit/specs/HelloWorld.spec.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import HelloWorld from '@/components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{#typescript}}import {expect} from 'chai'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}{{/typescript}}
+{{#typescript}}{{#if_eq runner "karma"}}import {expect} from 'chai'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}{{/if_eq}}{{/typescript}}
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
