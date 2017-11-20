d232576a19a99d6212f16b7f69a709a6c057dc56
c0defre4k <c0defre4k@users.noreply.github.com>
2017-11-20 18:25:33 +0100

    revert #996 chunk names to minify prodution JS (#1086)

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -26,7 +26,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[name].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
