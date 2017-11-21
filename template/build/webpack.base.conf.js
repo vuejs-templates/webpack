7cf7d4134607348f296287b661dfa8764880c605
Evan You <yyx990803@gmail.com>
2017-11-21 17:50:18 -0500

    prevent webpack from injecting useless mocks

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -78,5 +78,17 @@ module.exports = {
         }
       }
     ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
