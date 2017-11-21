5b138f3bd84694f6a6036c3a7252cfccc02688cf
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2017-11-21 09:23:51 -0500

    Simplify elementCount custom assertions (#898)
    * Improve custom Nightwatch assertion comment readability
    
    Standardize capitalization, punctuation
    Use common language
    Reduce word count
    
    * Clarify elementCount arguments
    
    Before this commit the term `selector` was used for two separate arguments in
    two separate scopes. Rename the function passed to the browser so the developer
    understands that these are two arguments/two scopes/not the same value.

--- template/test/e2e/custom-assertions/elementCount.js
+++ template/test/e2e/custom-assertions/elementCount.js
@@ -1,11 +1,12 @@
 // A custom Nightwatch assertion.
-// the name of the method is the filename.
-// can be used in tests like this:
+// The assertion name is the filename.
+// Example usage:
 //
 //   browser.assert.elementCount(selector, count)
 //
-// for how to write custom assertions see
+// For more information on custom assertions see:
 // http://nightwatchjs.org/guide#writing-custom-assertions
+
 exports.assertion = function (selector, count) {
   this.message = 'Testing if element <' + selector + '> has count: ' + count{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   this.expected = count{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
@@ -17,8 +18,8 @@ exports.assertion = function (selector, count) {
   }
   this.command = function (cb) {
     var self = this{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    return this.api.execute(function (selector) {
-      return document.querySelectorAll(selector).length{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    return this.api.execute(function (selectorToCount) {
+      return document.querySelectorAll(selectorToCount).length{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     }, [selector], function (res) {
       cb.call(self, res){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
