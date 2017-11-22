92df011915e2973068b34e01d7f482760356e648
y-kurami <yosuke.kurami@gmail.com>
2017-11-22 14:38:37 +0900

    use chrome launcher instead of PhantomJS

--- template/test/unit/karma.conf.js
+++ template/test/unit/karma.conf.js
@@ -11,8 +11,8 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    browsers: ['Chrome'],
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
