0d0c6866922ee1b074f82803d36134fe12abf236
suhao <valar_su@163.com>
2017-11-23 16:26:20 +0800

    bugfix

--- template/src/main.js
+++ template/src/main.js
@@ -22,11 +22,11 @@ Vue.use(VueAxios, axios){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-{{#vuex}}  //vuex为true的时候就会写入这些
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/vuex}}
+// {{#vuex}}  //vuex为true的时候就会写入这些
+// import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+// import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+// Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+// {{/vuex}}
 
 
 /* eslint-disable no-new */
@@ -35,9 +35,9 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
-  {{#vuex}}
-  store,
-  {{/vuex}}
+  // {{#vuex}}
+  // store,
+  // {{/vuex}}
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
