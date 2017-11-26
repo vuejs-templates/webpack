c325e9fc91c35794a722bd60ad2f2578e4763b2a
Robert Martin <rmartin@rmart.in>
2017-11-26 06:30:22 -0500

    Update meta.js (#1091)
    Minor style fix

--- meta.js
+++ meta.js
@@ -72,7 +72,7 @@ module.exports = {
     },
     "unit": {
       "type": "confirm",
-      "message": "Setup unit tests"
+      "message": "Set up unit tests"
     },
     "runner": {
       "when": "unit",
