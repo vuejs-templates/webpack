8c14c7dd1e69609f0d9812669d5f07c302192bd0
Thorsten <t.luenborg@googlemail.com>
2017-12-02 21:23:19 +0100

    removed special airbnb template strings since we can auto-fix them now.

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -25,10 +25,10 @@ export default {
   name: 'HelloWorld',
   data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
     return {
-      msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- template/test/e2e/runner.js
+++ template/test/e2e/runner.js
@@ -1,21 +1,21 @@
 // 1. start the dev server using production config
-process.env.NODE_ENV = 'testing'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+process.env.NODE_ENV = 'testing'
 
-const webpack = require('webpack'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-const DevServer = require('webpack-dev-server'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+const webpack = require('webpack')
+const DevServer = require('webpack-dev-server')
 
-const webpackConfig = require('../../build/webpack.prod.conf'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-const devConfigPromise = require('../../build/webpack.dev.conf'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+const webpackConfig = require('../../build/webpack.prod.conf')
+const devConfigPromise = require('../../build/webpack.dev.conf')
 
-let server{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+let server
 
 devConfigPromise.then(devConfig => {
-  const devServerOptions = devConfig.devServer{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  const compiler = webpack(webpackConfig){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  server = new DevServer(compiler, devServerOptions){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  const port = devServerOptions.port{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  const host = devServerOptions.host{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  return server.listen(port, host){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const devServerOptions = devConfig.devServer
+  const compiler = webpack(webpackConfig)
+  server = new DevServer(compiler, devServerOptions)
+  const port = devServerOptions.port
+  const host = devServerOptions.host
+  return server.listen(port, host)
 })
 .then(() => {
   // 2. run the nightwatch test suite against it
@@ -25,24 +25,24 @@ devConfigPromise.then(devConfig => {
   // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
   // For more information on Nightwatch's config file, see
   // http://nightwatchjs.org/guide#settings-file
-  let opts = process.argv.slice(2){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  let opts = process.argv.slice(2)
   if (opts.indexOf('--config') === -1) {
-    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
   }
   if (opts.indexOf('--env') === -1) {
-    opts = opts.concat(['--env', 'chrome']){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    opts = opts.concat(['--env', 'chrome'])
   }
 
-  const spawn = require('cross-spawn'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  const spawn = require('cross-spawn')
+  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
 
   runner.on('exit', function (code) {
-    server.close(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    process.exit(code){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    server.close()
+    process.exit(code)
+  })
 
   runner.on('error', function (err) {
-    server.close(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-    throw err{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    server.close()
+    throw err
+  })
+})
