590bdfa8e01c87a7f3d665f0ad01b8a7e0035711
Thorsten <t.luenborg@googlemail.com>
2017-12-02 21:26:39 +0100

    remove last airbnb template string

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -23,7 +23,7 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+  data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
