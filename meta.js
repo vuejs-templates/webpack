40cdf3acb198063b443703fa801724cb5effeb87
Thorsten <t.luenborg@googlemail.com>
2017-12-04 19:47:11 +0100

    change double quotes to single quotes, remove where possible

--- meta.js
+++ meta.js
@@ -8,8 +8,8 @@ const {
 } = require('./utils')
 
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
+  helpers: {
+    if_or: function (v1, v2, options) {
       if (v1 || v2) {
         return options.fn(this);
       }
@@ -17,105 +17,105 @@ module.exports = {
       return options.inverse(this);
     }
   },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name'
     },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "A Vue.js project"
+    description: {
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A Vue.js project'
     },
-    "author": {
-      "type": "string",
-      "message": "Author"
+    author: {
+      type: 'string',
+      message: 'Author'
     },
-    "build": {
-      "type": "list",
-      "message": "Vue build",
-      "choices": [
+    build: {
+      type: 'list',
+      message: 'Vue build',
+      choices: [
         {
-          "name": "Runtime + Compiler: recommended for most users",
-          "value": "standalone",
-          "short": "standalone"
+          name: 'Runtime + Compiler: recommended for most users',
+          value: 'standalone',
+          short: 'standalone'
         },
         {
-          "name": "Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere",
-          "value": "runtime",
-          "short": "runtime"
+          name: 'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
+          value: 'runtime',
+          short: 'runtime'
         }
       ]
     },
-    "router": {
-      "type": "confirm",
-      "message": "Install vue-router?"
+    router: {
+      type: 'confirm',
+      message: 'Install vue-router?'
     },
-    "lint": {
-      "type": "confirm",
-      "message": "Use ESLint to lint your code?"
+    lint: {
+      type: 'confirm',
+      message: 'Use ESLint to lint your code?'
     },
-    "lintConfig": {
-      "when": "lint",
-      "type": "list",
-      "message": "Pick an ESLint preset",
-      "choices": [
+    lintConfig: {
+      when: 'lint',
+      type: 'list',
+      message: 'Pick an ESLint preset',
+      choices: [
         {
-          "name": "Standard (https://github.com/standard/standard)",
-          "value": "standard",
-          "short": "Standard"
+          name: 'Standard (https://github.com/standard/standard)',
+          value: 'standard',
+          short: 'Standard'
         },
         {
-          "name": "Airbnb (https://github.com/airbnb/javascript)",
-          "value": "airbnb",
-          "short": "Airbnb"
+          name: 'Airbnb (https://github.com/airbnb/javascript)',
+          value: 'airbnb',
+          short: 'Airbnb'
         },
         {
-          "name": "none (configure it yourself)",
-          "value": "none",
-          "short": "none"
+          name: 'none (configure it yourself)',
+          value: 'none',
+          short: 'none'
         }
       ]
     },
-    "unit": {
-      "type": "confirm",
-      "message": "Set up unit tests"
+    unit: {
+      type: 'confirm',
+      message: 'Set up unit tests'
     },
-    "runner": {
-      "when": "unit",
-      "type": "list",
-      "message": "Pick a test runner",
-      "choices": [
+    runner: {
+      when: 'unit',
+      type: 'list',
+      message: 'Pick a test runner',
+      choices: [
         {
-          "name": "Jest",
-          "value": "jest",
-          "short": "jest"
+          name: 'Jest',
+          value: 'jest',
+          short: 'jest'
         },
         {
-          "name": "Karma and Mocha",
-          "value": "karma",
-          "short": "karma"
+          name: 'Karma and Mocha',
+          value: 'karma',
+          short: 'karma'
         },
         {
-          "name": "none (configure it yourself)",
-          "value": "noTest",
-          "short": "noTest"
+          name: 'none (configure it yourself)',
+          value: 'noTest',
+          short: 'noTest'
         }
       ]
     },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with Nightwatch?"
+    e2e: {
+      type: 'confirm',
+      message: 'Setup e2e tests with Nightwatch?'
     },
-    "autoInstall": {
-      "type": "list",
-      "message": "Should we run `npm install` for you after the project has been created? (recommended)",
-      "choices": [
+    autoInstall: {
+      type: 'list',
+      message: 'Should we run `npm install` for you after the project has been created? (recommended)',
+      choices: [
         {
-          "name": "Yes, use NPM",
-          "value": 'npm',
+          name: 'Yes, use NPM',
+          value: 'npm',
           short: 'npm'
         },
         {
@@ -131,27 +131,27 @@ module.exports = {
       ]
     }
   },
-  "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "build/webpack.test.conf.js": "unit && runner === 'karma'",
-    "test/unit/**/*": "unit",
-    "test/unit/index.js": "unit && runner === 'karma'",
-    "test/unit/jest.conf.js": "unit && runner === 'jest'",
-    "test/unit/karma.conf.js": "unit && runner === 'karma'",
-    "test/unit/specs/index.js": "unit && runner === 'karma'",
-    "test/unit/setup.js": "unit && runner === 'jest'",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+  filters: {
+    '.eslintrc.js': 'lint',
+    '.eslintignore': 'lint',
+    'config/test.env.js': 'unit || e2e',
+    'build/webpack.test.conf.js': "unit && runner === 'karma'",
+    'test/unit/**/*': 'unit',
+    'test/unit/index.js': "unit && runner === 'karma'",
+    'test/unit/jest.conf.js': "unit && runner === 'jest'",
+    'test/unit/karma.conf.js': "unit && runner === 'karma'",
+    'test/unit/specs/index.js': "unit && runner === 'karma'",
+    'test/unit/setup.js': "unit && runner === 'jest'",
+    'test/e2e/**/*': 'e2e',
+    'src/router/**/*': 'router'
   },
-  "complete": function (data, { chalk }) {
+  'complete': function (data, { chalk }) {
     
     const green = chalk.green
 
     sortDependencies(data, green)
 
-    const cwd = path.join(process.cwd(), data.inPlace ? "" : data.destDirName)
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
     
     if (data.autoInstall) {
       installDependencies(cwd, data.autoInstall, green)
