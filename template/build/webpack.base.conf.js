98a8064297684fab87605468afa38a3a6c37ee53
Rémi Alvergnat <toilal.dev@gmail.com>
2017-12-14 13:14:30 +0100

    Remove useless code when lint is not chosen (#1165)

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -8,7 +8,7 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const createLintingRule = () => ({
+{{#lint}}const createLintingRule = () => ({
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
   enforce: 'pre',
@@ -17,7 +17,7 @@ const createLintingRule = () => ({
     formatter: require('eslint-friendly-formatter'),
     emitWarning: !config.dev.showEslintErrorsInOverlay
   }
-})
+}){{/lint}}
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
