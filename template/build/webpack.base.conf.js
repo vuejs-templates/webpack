549078351ef381e9b4cdd73d7aa7a4d57534e4a2
Thorsten <t.luenborg@googlemail.com>
2017-12-15 17:04:44 +0100

    fix #1140

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -53,7 +53,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
