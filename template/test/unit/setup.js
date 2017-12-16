979b8da34c96f31fa296441176f5ff391fc0614f
Thorsten <t.luenborg@googlemail.com>
2017-12-16 11:06:49 +0100

    properly load env variables in jest setup

--- template/test/unit/setup.js
+++ template/test/unit/setup.js
@@ -1,10 +1,19 @@
 // setup file for jest unit tests.
 // this file is run before jest runs any of your tests.
-import env from '../../config/env' // this should always be loaded first!
 import Vue from 'vue'
 
-Vue.config.productionTip = false
+// We load any environment variable defined in a `.env` file first
+reuqire('dotenv').config()
 
+// Then we set NODE_ENV
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'test'
+}
+
+// then we load any other variables defined in the matching variabel file
+const env = require(`../../config/variables/${process.env.NODE_ENV}`)
 Object.keys(env).forEach(key => {
-  process.env[key] = JSON.parse(env[key]) // since we stringify in the env file, we parse it back here.
+  process.env[key] = env[key]
 })
+
+Vue.config.productionTip = false
