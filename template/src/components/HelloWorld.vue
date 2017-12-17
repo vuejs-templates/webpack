768b826a1c604f666812caccaaa414a4907383fd
Igor Tulmentyev <igtulm@gmail.com>
2017-12-18 01:24:59 +0400

    fix syntax for airbnb eslint

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -23,12 +23,12 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      component: HelloWorld,
+    },
+  ],
+});
