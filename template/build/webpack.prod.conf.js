cd4d7d957c9af3d37092c79bf490b56b8d88b108
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2017-12-17 19:54:55 +0100

    fix typo in a comment (#1183)

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -78,7 +78,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
-    // keep module.id stable when vender modules does not change
+    // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
