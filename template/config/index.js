1b7687130ca22f7f8f77499aeeca49aa81eb4c1c
Thorsten <t.luenborg@googlemail.com>
2017-12-31 14:33:14 +0100

    Make CSS sourcemaps on by default
    The problem described shouldn't even matter since it's a prob of style-loader, and we use vue-style-loader which had this fixed a long time ago

--- template/config/index.js
+++ template/config/index.js
@@ -41,12 +41,7 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false,
+    cssSourceMap: true,
   },
 
   build: {
