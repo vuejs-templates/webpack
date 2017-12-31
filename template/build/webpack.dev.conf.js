77f818fa7e1cad836d5d89164c2c93bccc4a5b96
Thorsten <t.luenborg@googlemail.com>
2017-12-31 14:31:24 +0100

    Ensure page reloads in history mode serve index.html
    After the switch to CopyWebpackPlugin (#1176), historyAPIFallback had to be adjusted to still serve index.html from all catched paths correctly.
    See: https://github.com/vuejs-templates/webpack/issues/1176#issuecomment-354347953

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -23,7 +23,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   // these devServer options should be customized in /config/index.js
   devServer: {
     clientLogLevel: 'warning',
-    historyApiFallback: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /.*/, to: path.join(config.dev.assetsPublicPath, 'index.html') },
+      ],
+    },
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
