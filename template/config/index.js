89998068807e71b674eeca51e91d55aa183adadc
Evan You <yyx990803@gmail.com>
2017-12-31 00:04:25 -0500

    use old sourcemap option

--- template/config/index.js
+++ template/config/index.js
@@ -34,7 +34,7 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
