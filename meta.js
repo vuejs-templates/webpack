fd65d24ad2ed5113dfb6790c600c51e6ea572cd8
Thorsten Lünborg <t.luenborg@googlemail.com>
2018-01-13 16:23:51 +0100

    Improve template CI tests (close #1218)
    * Batman!
    
    * fix wrong job name
    
    * fix working dir paths
    
    * fix node_modules path
    
    * test other step order
    
    * try other shell
    
    * ...and another one
    
    * rewrite test.sh
    
    * try running a bash script instead
    
    * and another attempt.
    
    * make executable
    
    * anpother try...
    
    * fix workflow errors
    
    * fix path
    
    * Batman!
    
    * added scenarios and made it work!!
    
    * refactors and comments
    
    * just fumbling around
    
    * adfda
    
    * adjust test scenarios for autoInstall
    
    * x<vb<xb
    
    * first try since a long time
    
    * small fix
    
    * fix path to vue-cli
    
    * fix scenarios
    
    * fix persist paths
    
    * testing with quotes for the path
    
    * removing unhelpful quotes
    
    * New Test
    
    * fixed indentation
    
    * test with full setup
    
    * 2nd full test
    
    * bump jest version
    
    * other docker image
    
    * test with runinband
    
    * test with airbnb/karma as well
    
    * remove superfluous checkout
    
    * run unit and e2e individually.
    
    * fix eslint paths for test subdirs
    
    * adjust for airbnb preset
    
    * only lint e2e specs, config differences are unfixable.
    
    * adding a small readme
    
    * remove test.sh
    
    not needed anymore

--- meta.js
+++ meta.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+
 const {
   sortDependencies,
   installDependencies,
@@ -10,9 +11,16 @@ const pkg = require('./package.json')
 
 const templateVersion = pkg.version
 
+const { addTestAnswers } = require('./scenarios')
+
 module.exports = {
+  metalsmith: {
+    // When running tests for the template, this adds answers for the selected scenario
+    before: addTestAnswers
+  },
   helpers: {
     if_or(v1, v2, options) {
+
       if (v1 || v2) {
         return options.fn(this)
       }
@@ -26,21 +34,25 @@ module.exports = {
   
   prompts: {
     name: {
+      when: 'isNotTest',
       type: 'string',
       required: true,
       message: 'Project name',
     },
     description: {
+      when: 'isNotTest',
       type: 'string',
       required: false,
       message: 'Project description',
       default: 'A Vue.js project',
     },
     author: {
+      when: 'isNotTest',
       type: 'string',
       message: 'Author',
     },
     build: {
+      when: 'isNotTest',
       type: 'list',
       message: 'Vue build',
       choices: [
@@ -58,15 +70,17 @@ module.exports = {
       ],
     },
     router: {
+      when: 'isNotTest',
       type: 'confirm',
       message: 'Install vue-router?',
     },
     lint: {
+      when: 'isNotTest',
       type: 'confirm',
       message: 'Use ESLint to lint your code?',
     },
     lintConfig: {
-      when: 'lint',
+      when: 'isNotTest && lint',
       type: 'list',
       message: 'Pick an ESLint preset',
       choices: [
@@ -88,11 +102,12 @@ module.exports = {
       ],
     },
     unit: {
+      when: 'isNotTest',
       type: 'confirm',
       message: 'Set up unit tests',
     },
     runner: {
-      when: 'unit',
+      when: 'isNotTest && unit',
       type: 'list',
       message: 'Pick a test runner',
       choices: [
@@ -114,10 +129,12 @@ module.exports = {
       ],
     },
     e2e: {
+      when: 'isNotTest',
       type: 'confirm',
       message: 'Setup e2e tests with Nightwatch?',
     },
     autoInstall: {
+      when: 'isNotTest',
       type: 'list',
       message:
         'Should we run `npm install` for you after the project has been created? (recommended)',
---
+++ scenarios/README.md
@@ -0,0 +1,27 @@
+## What is this folder?
+
+This folder contains test scenarios for the automated tests of the template on CircleCI.
+
+Each `.json`file contains an object that represents a set of answers to the questions that vue-cli asks the user when installing the template.
+
+With the code from `index.js`, we insert those answers into the metalsmith metadata and skip the inquirer questions, thus allowing us to run different test scenarios in CI without having to actually provide any answers to inquirer or to mock it.
+
+## The scenarios
+
+We currently have 3 scenrios set up:
+
+1. 'minimal': it basically answers "no" to ever choice, so no router, no elint, no tests
+2. 'full': It answers "yes" to every choice. With router, with linting (standard), with full tests (jest & e2e)
+3. 'full-airbnb-karma': like 'full', but using airbnb eslint config instead od standard and karma instead of jest for unnit tests.
+
+Other permutations might be worth testing to secure against edge cases, but this gives us a decent level of security over common combinations.
+
+## How to use it?
+
+Choosing a scenario is done through setting an ENV variable named `VUE_TEMPL_TEST`.
+
+You can run a scenario yourself by running this in your terminal:
+
+````
+VUE_TEMPL_TEST=minimal vue init webpack your-directory
+```
