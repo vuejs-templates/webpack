321773cc36b72b6b17ab7a4a3ade1e293d401e41
Leon Hyoung <574857444@qq.com>
2018-01-30 15:46:35 +0800

    添加collapse组件，更新scss样式结构，样式表

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -6,13 +6,13 @@
 var gulp = require('gulp'),
       smushit = require('gulp-smushit');
       const path = [
-        'src/assets/',//assets下的图片压缩
-        'static/'//static下的图片压缩
+        'src/assets/img',//assets下的图片压缩
+        'static/img'//static下的图片压缩
       ]
 
 gulp.task('min-img', function () {
   path.forEach(function (p1, p2, p3) {
-    gulp.src(p1 + '/*.{png,jpg,gif,ico}')
+    gulp.src(p1 + '/**/*.{png,jpg,gif,ico}')
       .pipe(smushit({
         verbose: true
       }))
--- template/src/App.vue
+++ template/src/App.vue
@@ -21,14 +21,3 @@ export default {
   }{{/router}}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
