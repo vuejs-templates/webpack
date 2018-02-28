5e54dedf9bcf5ff29ddc33f6d2eafb8ef6dd8e90
admin <admin>
2018-02-28 14:36:55 +0800

    取消ESLint

--- meta.js
+++ meta.js
@@ -100,28 +100,28 @@ module.exports = {
     //   type: 'confirm',
     //   message: 'Use ESLint to lint your code?',
     // },
-    lintConfig: {
-      when: 'isNotTest && lint',
-      type: 'list',
-      message: 'Pick an ESLint preset',
-      choices: [
-        {
-          name: 'Standard (https://github.com/standard/standard)',
-          value: 'standard',
-          short: 'Standard',
-        },
-        {
-          name: 'Airbnb (https://github.com/airbnb/javascript)',
-          value: 'airbnb',
-          short: 'Airbnb',
-        },
-        {
-          name: 'none (configure it yourself)',
-          value: 'none',
-          short: 'none',
-        },
-      ],
-    },
+    // lintConfig: {
+    //   when: 'isNotTest && lint',
+    //   type: 'list',
+    //   message: 'Pick an ESLint preset',
+    //   choices: [
+    //     {
+    //       name: 'Standard (https://github.com/standard/standard)',
+    //       value: 'standard',
+    //       short: 'Standard',
+    //     },
+    //     {
+    //       name: 'Airbnb (https://github.com/airbnb/javascript)',
+    //       value: 'airbnb',
+    //       short: 'Airbnb',
+    //     },
+    //     {
+    //       name: 'none (configure it yourself)',
+    //       value: 'none',
+    //       short: 'none',
+    //     },
+    //   ],
+    // },
     unit: {
       when: 'isNotTest',
       type: 'confirm',
