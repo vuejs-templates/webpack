6add5693d1578c3759b2df4d9ebf43580c94f1af
zysdo <32849335@qq.com>
2018-03-30 15:24:24 +0800

    更改router为懒加载的写法

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,7 +8,9 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component(resolve) {
+        require(['@/components/HelloWorld'], resolve);
+      }
     }
   ]
 })
