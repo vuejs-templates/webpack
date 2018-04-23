8b60bd31e0647669efb1d633accdce3bd3481973
Roger <narayanroger@gmail.com>
2018-04-23 09:50:47 -0400

    Add support for building documentation with VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'Webpack',
+    description: "A full-featured Webpack + vue-loader setup with hot reload, linting, testing & css extraction.",
+    themeConfig: {
+      repo: 'vuejs-templates/webpack',
+      editLinks: true,
+      docsDir: 'docs',
+      sidebar: [
+        '/structure',
+        '/commands',
+        '/babel',
+        '/linter',
+        '/pre-processors',
+        '/static',
+        '/env',
+        '/backend',
+        '/proxy',
+        '/unit',
+        '/e2e',
+        '/prerender'
+    ]
+    }
+  }
