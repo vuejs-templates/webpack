8d638f077465367460421b6b6b5f9fc17f78b4c6
Noam Kfir <noam@kfir.cc>
2018-04-28 12:54:15 +0300

    Updated nightwatch to 1.0.3 beta. Attempt to fix failing build.

--- template/test/e2e/nightwatch.conf.js
+++ template/test/e2e/nightwatch.conf.js
@@ -1,45 +1,87 @@
 require('babel-register')
 var config = require('../../config')
 
+const paths = {
+  chromedriver: require('chromedriver').path,
+  geckodriver: require('geckodriver').path,
+  seleniumServer: require('selenium-server').path,
+  launchUrl: 'http://localhost:' + (process.env.PORT || config.dev.port)
+}
+console.log('paths:', paths)
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
+  detailed_output: true,
+  silent: false,
+  launch_url: paths.launchUrl,
+  filter: '*.js',
 
-  selenium: {
+  webdriver: {
     start_process: true,
-    server_path: require('selenium-server').path,
-    host: '127.0.0.1',
-    port: 4444,
-    cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path
-    }
+    server_path: paths.seleniumServer,
+    host: 'localhost',
+    port: 4444
   },
 
   test_settings: {
-    default: {
-      selenium_port: 4444,
-      selenium_host: 'localhost',
-      silent: true,
-      globals: {
-        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
-      }
-    },
+    // selenium_server: {
+    //   start_session: true,
+    //   selenium: {
+    //     start_process: true,
+    //     host: 'localhost',
+    //     port: 4444,
+    //     server_path: paths.seleniumServer,
+    //     silent: true,
+    //     cli_args: {
+    //       'webdriver.chrome.driver': paths.chromedriver,
+    //       'webdriver.gecko.driver': paths.geckodriver,
+    //       'webdriver.firefox.profile': ''
+    //     },
+    //     desiredCapabilities: {
+    //       browserName: 'chrome'
+    //     }
+    //   }
+    // },
 
     chrome: {
+      start_session: true,
+      webdriver: {
+        start_process: true,
+        server_path: paths.chromedriver,
+        port: 9515,
+        cli_args: [
+          '--verbose'
+        ]
+      },
       desiredCapabilities: {
         browserName: 'chrome',
         javascriptEnabled: true,
-        acceptSslCerts: true
+        acceptSslCerts: true,
+        loggingPrefs: {
+          driver: 'INFO',
+          server: 'OFF',
+          browser: 'INFO'
+        }
       }
     },
 
     firefox: {
+      start_session: true,
+      webdriver: {
+        start_process: true,
+        server_path: paths.geckodriver,
+        cli_args: [
+          '--log', 'debug'
+        ]
+      },
       desiredCapabilities: {
         browserName: 'firefox',
         javascriptEnabled: true,
-        acceptSslCerts: true
+        acceptSslCerts: true,
+        acceptInsecureCerts: true,
+        marionette: true
       }
     }
   }
