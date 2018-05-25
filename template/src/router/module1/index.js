3499da71e61aa23bb3e57de64fc4508830f6f8a8
Helio S. Junior <hj_junior@hotmail.com>
2018-05-25 12:37:14 -0300

    Fixed problem to export

--- template/src/router/module1/index.js
+++ template/src/router/module1/index.js
@@ -1,9 +1,11 @@
 import HelloWorld from '@/components/HelloWorld'
 
-export default Module1 = [
+const Module1 = [
   {
     path: '/',
     name: 'HelloWorld',
     component: HelloWorld
   }
 ]
+
+export default Module1
