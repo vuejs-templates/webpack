eb596aea2641f0222ba1f8b55c07441f5f330523
wujun <wu.jun@burgeon.cn>
2018-08-24 15:44:47 +0800

    123

--- template/src/api/index.js
+++ template/src/api/index.js
@@ -1,20 +1,8 @@
-import axios from "axios";
-var instance = axios.create({});
+import axios from 'axios';
+
+const instance = axios.create({});
 instance.interceptors.request.use(
-  function(config) {
-    let url = config.url;
-    if (url.indexOf("login") > -1) {
-      // sessionStorage.setItem("uid", "");
-      // config.headers.token = "";
-    } else {
-      config.headers.uid = sessionStorage.getItem("uid");
-      //config.headers.uid = "ofgdy0eVfFbCwZCiaPsniPR0KeRo";
-      console.log("当前请求的uid为:" + config.headers.uid);
-    }
-    return config;
-  },
-  function(err) {
-    return Promise.reject(err);
-  }
+  config => config,
+  err => Promise.reject(err),
 );
 export default instance;
