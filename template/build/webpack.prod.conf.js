7b848318833227f358b395ea2a180f9af30daaa4
luozhifeng <luozhifeng@tuhu.cn>
2018-09-30 14:12:16 +0800

    脚手架整理 2018/9/21

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -11,7 +11,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = require('../config/prod.env'
+const env = require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
