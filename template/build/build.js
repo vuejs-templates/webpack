151e8a8950a396713a5186110fc4e2b5a66d1890
y-io <me@yauri.io>
2018-10-21 19:37:02 +0800

    allow build for different NODE_ENV

--- template/build/build.js
+++ template/build/build.js
@@ -1,7 +1,7 @@
 'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const ora = require('ora')
 const rm = require('rimraf')
