c7f614a97fe9fbf9874dfb52e0fe517374544f8a
bhabgs <bhabgs@appledeMac-mini.local>
2018-11-09 13:56:24 +0800

    修改错别子

--- template/src/config/plugins.js
+++ template/src/config/plugins.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import utils from './util'; // 引入util 工具 
 import '@/assets/reset.scss'; // 引入重置css
 import '@/components'; // 引入自动加载组件
-{{#mick}}
+{{#mock}}
 require('@/mock');
 {{/mock}}
 Vue.prototype.$util = utils;
