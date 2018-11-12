250244eb82f8c1790df617b710a3ac3413e83b78
bhabgs <bhabgs@appledeMac-mini.local>
2018-11-12 11:03:41 +0800

    优化import

--- template/src/config/plugins.js
+++ template/src/config/plugins.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import '@/assets/reset.scss'; // 引入重置css
-import '@/components/index.js'; // 引入自动加载组件
-import util from './util.js'; // 引入util 工具 
+import '@/components'; // 引入自动加载组件
+import util from '@/config/util'; // 引入util 工具 
 {{#mock}}
 require('@/mock');
 {{/mock}}
