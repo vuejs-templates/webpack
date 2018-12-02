245b6e9aeb5780f65bd629fee4ba7fa379713701
ariussssss <1059146811@qq.com>
2018-12-02 16:28:30 +0800

    test

---
+++ template/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped
+  {{#stylus}}
+  lang="stylus" rel="stylesheet/stylus"
+  {{/stylus}}>
+</style>
---
+++ template/src/view/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>  
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped
+	{{#stylus}}
+	lang="stylus" rel="stylesheet/stylus"
+  {{/stylus}}>
+</style>
