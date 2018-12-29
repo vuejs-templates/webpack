05054f9d88e9b676a7b699f83b9e801ea150623a
YingDeYu <yyd-yyd@hotmail.com>
2018-12-29 17:06:31 +0800

    add /src/components/index.js

---
+++ template/src/components/index.js
@@ -0,0 +1,16 @@
+import Menu from './Menu'
+import Header from './Header'
+import Breadcrumb from './Breadcrumb'
+import Title from './Title'
+import Pagination from './Pagination'
+import BannerView from './BannerView'
+
+export {
+    Menu,
+    Header,
+    Breadcrumb,
+    Title,
+    Pagination,
+    BannerView,
+    Tabs
+}
