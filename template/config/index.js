6e50dfe0c82e05667a3501db26694fcbb6c7de31
王彬宇 <wangby_bcc@si-tech.com.cn>
2021-05-15 01:45:21 +0800

    refactor(custom fix):
    fix Mismatched tags

--- template/config/index.js
+++ template/config/index.js
@@ -27,6 +27,7 @@ module.exports = {
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
+    {{/lint}}
 
     /**
      * Source Maps
